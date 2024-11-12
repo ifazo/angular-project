ca49aac7a794c284fc9305da4d6a8c359164cb9c
Ziaul Karim Ifaz <ziaulkarimifaz@outlook.com>
2024-11-12 16:47:04 +0000

    header functionality done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { CategoryOverviewComponent } from '../../components/category-overview/category-overview.component';
-import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, CategoryOverviewComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
