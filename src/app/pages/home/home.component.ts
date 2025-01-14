8b15469d89dac48b7369472dc8479b63ef55d85b
Ziaul Karim Ifaz <ziaulkarimifaz@outlook.com>
2025-01-14 13:23:20 +0000

    newsletter add

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { FeatureProductsComponent } from "../../components/feature-products/feature-products.component";
+import { NewsletterComponent } from '../../components/newsletter/newsletter.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, FeatureProductsComponent],
+  imports: [HeroComponent, FeatureProductsComponent, NewsletterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
